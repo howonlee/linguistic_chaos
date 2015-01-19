60d112445861542b979fe238c172562d04961e5e
Howon Lee <howon@howonlee.com>
2015-01-18 20:07:03 -0800

    start the game

---
+++ game.py
@@ -0,0 +1,19 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+
+def next_idx(idx):
+    next_ifs = random.choice([0,1,2])
+    if random
+
+def animate(i):
+    next_idx = someythin
+
+if __name__ == "__main__":
+    frac = np.zeros((1000, 1000))
+    last_idx = (random.randint(0,999), random.randint(0,999))
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0,1000), ylim=(0,1000))
+
+    print frac, last_idx
