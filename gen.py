064365a79990bfef6ec06b69b47315241b6efc09
Howon Lee <howon@howonlee.com>
2015-01-21 01:22:44 -0800

    start worrying about the histogram data

--- gen.py
+++ gen.py
@@ -1,10 +1,19 @@
 from fbm_stats import generate_fbm
 import numpy as np
+import collections
+import operator
 import matplotlib.pyplot as plt
 
-if __name__ == "__main__":
+def gen_and_save_fbm():
     fbm = generate_fbm(10000)
     bins = np.linspace(0, 1, 1000)
     np.save("digitized_fbm", np.digitize(fbm, bins))
-    #plt.plot(generate_fbm())
-    #plt.show()
+
+def load_fbm(name="digitized_fbm.npy"):
+    return np.load(name)
+
+if __name__ == "__main__":
+    fbm = collections.Counter(list(load_fbm()))
+    common = fbm.most_common()
+    plt.hist(map(operator.itemgetter(1), common))
+    plt.show()
